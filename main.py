8a68a6b934f94de7d47407126311dd2f8ffb68e7
Pernille3000 <p@pernille3000.dk>
2024-04-23 12:14:51 +0200

    Printer første jobtitel i konsollen

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
-import webbrowser
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 def main():
+    # Launch Chrome with the specified options
+    driver = webdriver.Chrome()
+
     # Åbn hjemmesiden
     url = "https://midtjob.dk/ledige-jobs"
-    webbrowser.open(url)
+    
+    # Åbn en Chrome-browser ved hjælp af Selenium
+    driver.get(url)
+    
+    # Find titel-elementet ved hjælp af XPath
+    titel = driver.find_element(By.XPATH, "/html/body/div[1]/div[1]/div[2]/div[3]/div[3]/div[2]/div[1]/a")
+    print(titel.text)
 
-
+    # Luk browseren
+    driver.quit()
 
 if __name__ == "__main__":
     main()
